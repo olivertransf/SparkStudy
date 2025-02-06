c8938e07c89c78526aa335067c2e003ce56c3a04
olivar <olivertransf@gmail.com>
2025-02-05 21:55:40 -0800

    Initial Commit

---
+++ SparkStudyTests/SparkStudyTests.swift
@@ -0,0 +1,16 @@
+//
+//  SparkStudyTests.swift
+//  SparkStudyTests
+//
+//  Created by Oliver Tran on 2/5/25.
+//
+
+import Testing
+
+struct SparkStudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
